d0a9f4fcaeaa7b06d68b66fd522cf316f2d606c8
Nauman iftikhar <naumandk1@gmail.com>
2025-06-09 23:02:59 +0200

    ✅ Added Customer model and updated AppDbContext

--- ProductStoreApi/Model/Customer.cs
+++ ProductStoreApi/Model/Customer.cs
@@ -2,5 +2,15 @@
 {
     public class Customer
     {
+
+        public int Id { get; set; } // Primary key
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
